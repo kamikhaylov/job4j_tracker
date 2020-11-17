4333ceed625ad3c96ea0c3e85e433e16579a76c0
konstantin <kotmix@bk.ru>
2020-11-17 23:07:00 +0400

    7. Метод удаления заявки Tracker.delete [#401322]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -44,6 +44,18 @@ public class Tracker {
         return result;
     }
 
+    public boolean delete(int id)  {
+        boolean result = false;
+        int index = indexOf(id);
+        if (index != -1) {
+            System.arraycopy(items, index + 1, items, index, size - index);
+            items[size - 1] = null;
+            size--;
+            result = true;
+        }
+        return result;
+    }
+
     private int indexOf(int id) {
         int result = -1;
         for (int index = 0; index < size; index++) {
