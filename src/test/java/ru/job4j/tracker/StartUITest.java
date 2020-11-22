f1fc79c70192184c2c04902219e563a7018647d5
konstantin <kotmix@bk.ru>
2020-11-22 21:19:46 +0400

    1.1. Тесты на StartUI. [#401271]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import ru.job4j.tracker.actions.*;
 import ru.job4j.tracker.input.Input;
 import ru.job4j.tracker.input.StubInput;
-import ru.job4j.tracker.input.ValidateInput;
 import ru.job4j.tracker.output.ConsoleOutput;
 import ru.job4j.tracker.output.Output;
 import ru.job4j.tracker.output.StubOutput;
