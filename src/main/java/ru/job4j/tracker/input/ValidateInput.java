b842a3dfd1df3854530e099d9ad16fb1c6cbb9e5
konstantin <kotmix@bk.ru>
2020-11-22 22:28:48 +0400

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#401263]

--- src/main/java/ru/job4j/tracker/input/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/input/ValidateInput.java
@@ -1,16 +1,31 @@
 package ru.job4j.tracker.input;
 
-public class ValidateInput extends ConsoleInput {
+import ru.job4j.tracker.output.Output;
+
+public class ValidateInput implements Input {
+    private final Output out;
+    private final Input in;
+
+    public ValidateInput(Output out, Input input) {
+        this.out = out;
+        this.in = input;
+    }
+
+    @Override
+    public String askStr(String question) {
+        return in.askStr(question);
+    }
+
     @Override
     public int askInt(String question) {
         boolean invalid = true;
         int value = -1;
         do {
             try {
-                value = super.askInt(question);
+                value = in.askInt(question);
                 invalid = false;
             } catch (NumberFormatException nfe) {
-                System.out.println("Please enter validate data again.");
+                out.println("Please enter validate data again.");
             }
         } while (invalid);
         return value;
---
+++ src/test/java/ru/job4j/tracker/input/ValidateInputTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.tracker.input;
+
+import org.junit.Test;
+import ru.job4j.tracker.output.Output;
+import ru.job4j.tracker.output.StubOutput;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ValidateInputTest {
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu: ");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenOutput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        input.askInt("Enter menu: ");
+        assertThat(out.toString(), is("Please enter validate data again."
+                + System.lineSeparator()));
+    }
+}
