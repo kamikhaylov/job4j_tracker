0bb65aabfac83d09f75c925829b4cc1201d095ca
konstantin <kotmix@bk.ru>
2021-09-19 22:09:39 +0400

    4.4. Liquibase. Интеграционные тесты. Proxy. [#95881] Fix

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,8 +6,6 @@ import ru.job4j.tracker.input.Input;
 import ru.job4j.tracker.input.ValidateInput;
 import ru.job4j.tracker.output.ConsoleOutput;
 import ru.job4j.tracker.output.Output;
-
-import java.sql.Connection;
 import java.util.List;
 
 public class StartUI {
