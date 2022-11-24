6a8409884b718d95954b55e0a49bc477dbb0a25b
konstantin <kotmix@bk.ru>
2022-11-24 23:33:53 +0400

    2. @Setter, @Getter и другие [#504889 #364063]

---
+++ src/main/java/ru/job4j/tracker/Category.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker;
+
+import lombok.*;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@RequiredArgsConstructor
+public class Category {
+    @NonNull
+    @Getter
+    @EqualsAndHashCode.Include
+    private int id;
+    @Getter
+    @Setter
+    private String name;
+}
