7c2a617f1dd934e2b3b4e237e66ed188cabe4166
konstantin <kotmix@bk.ru>
2022-12-04 11:49:14 +0400

    1. Конфигурация Hibernate [#6872 #364914] fix

--- src/main/java/ru/job4j/tracker/HibernateRun.java
+++ src/main/java/ru/job4j/tracker/HibernateRun.java
@@ -65,7 +65,7 @@ public class HibernateRun {
     public static List<Item> findAll(SessionFactory sf) {
         Session session = sf.openSession();
         session.beginTransaction();
-        List<Item> result = session.createQuery("FROM item", Item.class).list();
+        List<Item> result = session.createQuery("SELECT i FROM Item i", Item.class).list();
         session.getTransaction().commit();
         session.close();
         return result;
